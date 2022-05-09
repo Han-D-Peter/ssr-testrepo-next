7d81069dc4d4869571b6db4a0f31e41efd236130
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2022-05-10 01:46:26 +0900

    feat: useOnClickOutside hook 추가

---
+++ src/domains/shared/hooks/useOnClickOutside.ts
@@ -0,0 +1,31 @@
+import { RefObject, useEffect, useState } from 'react';
+
+export function useOnClickOutside(ref: RefObject<HTMLElement> | undefined, callback: (event: Event) => void) {
+  const [isTouchEvent, setTouchEvent] = useState(false);
+  const eventType = isTouchEvent ? 'touchend' : 'click';
+
+  function handleEvent(event: Event) {
+    if (event.type === 'click' && isTouchEvent) {
+      return;
+    }
+
+    if (ref?.current && event.target !== null) {
+      if (!ref.current.contains(event.target as Node)) {
+        event.stopPropagation();
+        callback(event);
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener(eventType, handleEvent, true);
+
+    return () => {
+      document.removeEventListener(eventType, handleEvent, true);
+    };
+  });
+
+  useEffect(() => {
+    setTouchEvent('ontouchstart' in document.documentElement);
+  }, []);
+}
