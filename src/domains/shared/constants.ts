71e0ad3a950e9d2a1203be90eec97cf1b8db3171
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2022-05-13 02:44:24 +0900

    feat: 디자인 시스템 기본 컬러 값 설정

---
+++ src/domains/shared/constants.ts
@@ -0,0 +1,23 @@
+export const Color = {
+  Primary100: '#0016D9',
+  Primary80: '#3C6EEB',
+  Primary50: '#6DB5F2',
+  Primary20: '#8FE8F7',
+
+  Gray100: '#A2A5AD',
+  Gray80: '#BDBEC6',
+  Gray50: '#CBCDD6',
+  Gray30: '#E5E6EC',
+  Gray10: '#F7F7F7',
+
+  Black100: '#191A1C',
+  Black80: '#303136',
+  Black50: '#464851',
+  Black30: '#585B67',
+  Black10: '#676A75',
+  Black0: '#8C8F9C',
+
+  White100: '#fff',
+};
+
+export type ColorType = keyof typeof Color;
