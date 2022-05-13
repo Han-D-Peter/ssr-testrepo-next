a6f883da9d7a218861544a531a27acb24bd8197e
Han-D-Peter <secret_wings@naver.com>
2022-05-13 14:43:47 +0900

    chore: 전역 css normalize 구성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { css, Global } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+        `}
+      />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
