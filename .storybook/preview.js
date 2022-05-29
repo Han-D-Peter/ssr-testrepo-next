b7ffccad5994a17e587b21eee44d27176b5d5a45
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2022-05-29 23:28:51 +0900

    feat: Storybook GlobalStyle 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import GlobalStyle from '../src/style/GlobalStyle';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
