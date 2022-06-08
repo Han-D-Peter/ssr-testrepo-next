497ac9559ad78674a939a9e4ad27275dfeffd3b5
Han, Jaesung <jaesunghan@soosungeng.com>
2022-06-08 17:50:06 +0900

    feat: 텍스트인풋 추가

--- src/domains/shared/components/Header/Header.tsx
+++ src/domains/shared/components/Header/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Logo from '../Logo';
-import Button from '../Button';
+import Button from '../button';
+import { Color } from '../../constants';
 import styled from '@emotion/styled';
+import TextInput from '../TextInput';
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Logo />
-      <Button color="Gray010">로그인</Button>
+      <SearchBarContainer>
+        <TextInput placeholder="검색어를 입력하세요" variant="search" width="327px" />
+      </SearchBarContainer>
+      <Button color="Gray800" textColor="Primary100" size="small">
+        로그인
+      </Button>
     </HeaderContainer>
   );
 };
@@ -15,9 +22,14 @@ const Header = () => {
 export default Header;
 
 const HeaderContainer = styled.header`
+  background-color: ${Color.Gray900};
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 64px;
   padding: 0 92px;
+`;
+
+const SearchBarContainer = styled.div`
+  width: 327px;
 `;
