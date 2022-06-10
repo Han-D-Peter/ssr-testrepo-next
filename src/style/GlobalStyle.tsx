e520ad45e94bdc3713a5b0d012b2450a5ef32e90
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2022-06-11 01:59:54 +0900

    style: GlobalStyle 추가

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,15 +1,27 @@
 import { css, Global } from '@emotion/react';
 import emotionNormalize from 'emotion-normalize';
+import { Color } from 'src/domains/shared/constants';
+import { GlobalEditorStyle } from './GlobalEditorStyle';
+import { GlobalModalStyle } from './GlobalModalStyle';
 
 const GlobalStyle = () => (
   <Global
     styles={css`
-      ${emotionNormalize}
       * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
       }
+      html,
+      body,
+      #__next {
+        height: 100%;
+        background-color: ${Color.Gray900};
+      }
+
+      ${emotionNormalize}
+      ${GlobalEditorStyle}
+      ${GlobalModalStyle}
     `}
   />
 );
