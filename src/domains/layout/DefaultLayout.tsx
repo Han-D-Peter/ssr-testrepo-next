43986e2f7837cc2079101699f86aee5cedae0c16
geonwoo-mun <geonwoo.mun@naverz-corp.com>
2022-06-11 02:01:58 +0900

    feat: DefaultLayout 추가

--- src/domains/layout/DefaultLayout.tsx
+++ src/domains/layout/DefaultLayout.tsx
@@ -1,15 +1,20 @@
+import styled from '@emotion/styled';
 import { PropsWithChildren } from 'react';
-import Footer from '../shared/components/Footer/Footer';
-import Header from '../shared/components/Header';
+import { Header } from '../shared/components';
 
 const DefaultLayout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
-      <Footer />
+      <LayoutMain>{children}</LayoutMain>
+      {/* <Footer /> */}
     </>
   );
 };
 
 export default DefaultLayout;
+
+const LayoutMain = styled.main`
+  min-height: calc(100% - 64px);
+  padding: 0 92px;
+`;
