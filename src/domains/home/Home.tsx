04ffc6a23c14a4e63a1427f7702c34a47a0dd7bb
Peter, Han <57760806+Han-D-Peter@users.noreply.github.com>
2022-06-13 17:36:27 +0900

    fix: 홈화면에서 템플릿 컴포넌트화 및 분리

--- src/domains/home/Home.tsx
+++ src/domains/home/Home.tsx
@@ -1,41 +1,81 @@
 import { css } from '@emotion/react';
-import Router from 'next/router';
 import React from 'react';
+import { PostGrid } from '../shared/components/PostGrid';
 import { useGetPostsQuery } from './Home.queries';
 
 const Home = () => {
   const getPostsQuery = useGetPostsQuery();
 
+  /** 목업 데이터 */
+  const postsMock = {
+    content: [
+      {
+        postIdx: 1,
+        title: 'server-side rendering',
+        contents: 'server-side rendering 은 ~~',
+        thumbnailImg: 'http://upload2.inven.co.kr/upload/2019/12/27/bbs/i14210693079.jpg',
+        thumbnailContents: 'server-side rendering 은',
+        boardCount: 15,
+        privated: 'N',
+        dateTime: '2022-06-09 00:01:37',
+        deleted: 'N',
+      },
+      {
+        postIdx: 2,
+        title: 'server-side rendering',
+        contents: 'server-side rendering 은 ~~',
+        thumbnailImg: 'http://upload2.inven.co.kr/upload/2019/12/27/bbs/i14210693079.jpg',
+        thumbnailContents: 'server-side rendering 은',
+        boardCount: 15,
+        privated: 'N',
+        dateTime: '2022-06-09 00:01:37',
+        deleted: 'N',
+      },
+      {
+        postIdx: 3,
+        title: 'server-side rendering',
+        contents: 'server-side rendering 은 ~~',
+        thumbnailImg: 'http://upload2.inven.co.kr/upload/2019/12/27/bbs/i14210693079.jpg',
+        thumbnailContents: 'server-side rendering 은',
+        boardCount: 15,
+        privated: 'N',
+        dateTime: '2022-06-09 00:01:37',
+        deleted: 'N',
+      },
+      {
+        postIdx: 4,
+        title: 'server-side rendering',
+        contents: 'server-side rendering 은 ~~',
+        thumbnailImg: 'http://upload2.inven.co.kr/upload/2019/12/27/bbs/i14210693079.jpg',
+        thumbnailContents: 'server-side rendering 은',
+        boardCount: 15,
+        privated: 'N',
+        dateTime: '2022-06-09 00:01:37',
+        deleted: 'N',
+      },
+      {
+        postIdx: 5,
+        title: 'server-side rendering',
+        contents: 'server-side rendering 은 ~~',
+        thumbnailImg: 'http://upload2.inven.co.kr/upload/2019/12/27/bbs/i14210693079.jpg',
+        thumbnailContents: 'server-side rendering 은',
+        boardCount: 15,
+        privated: 'N',
+        dateTime: '2022-06-09 00:01:37',
+        deleted: 'N',
+      },
+    ],
+  };
+
   return (
-    <div>
-      {getPostsQuery.data?.content.map((post) => (
-        <div
-          css={postWrapperStyle}
-          key={post.postIdx}
-          onClick={() => {
-            Router.push(
-              {
-                pathname: `/posts/[postIdx]`,
-                query: {
-                  postIdx: `${post.postIdx}`,
-                },
-              },
-              `/posts/${post.postIdx}`,
-            );
-          }}
-        >
-          <div>title: {post.title}</div>
-          <div>contents: {post.thumbnailContents}</div>
-        </div>
-      ))}
+    <div css={HomeGridLayout}>
+      <PostGrid contents={postsMock.content} columnCounts={4} />
     </div>
   );
 };
 
-export default Home;
-
-const postWrapperStyle = css`
-  border: 1px solid white;
-  color: white;
-  cursor: pointer;
+const HomeGridLayout = css`
+  margin-top: 131px;
 `;
+
+export default Home;
