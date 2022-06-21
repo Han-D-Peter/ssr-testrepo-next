290babea32edf325b1eaeb99bc38daa7b9648e1c
Peter, Han <57760806+Han-D-Peter@users.noreply.github.com>
2022-06-21 12:33:36 +0900

    feat: 토큰 만료시 리스페쉬 사용 기능

--- src/domains/shared/api/client.ts
+++ src/domains/shared/api/client.ts
@@ -9,12 +9,43 @@ const axios = Axios.create({
   },
 });
 
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    if (status === 401) {
+      const originalRequest = config;
+      const refreshToken = localStorage.getItem('refreshToken');
+      // refresh토큰 만료 상황로직 작성예정
+      try {
+        const { data } = await client.post('/refresh-token', { refreshToken });
+        const { token } = data;
+        setAuthToken(token);
+        return axios(originalRequest);
+      } catch (e) {
+        clearAuthToken();
+        localStorage.removeItem('refreshToken');
+        return Promise.reject(e);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
 export function setAuthToken(authToken: string): void {
   axios.defaults.headers.common['Authorization'] = `Bearer ${authToken}`;
+  window.sessionStorage.setItem('userInfo', authToken);
 }
 
 export function clearAuthToken(): void {
   axios.defaults.headers.common['Authorization'] = '';
+  window.sessionStorage.removeItem('userInfo');
 }
 
 const client = {
